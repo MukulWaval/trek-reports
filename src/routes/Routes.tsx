8d0f2fb4e7b74a36117a77e9dd777df143d148b1
MukulWaval <mukulwaval2000@gmail.com>
2024-12-19 22:07:48 +0530

    Update # based routing

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,29 +1,65 @@
+// import React from "react";
+// import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+// import Layout from "../components/Layout";
+// import NotFound from "../components/NotFound";
+// import Home from "../pages/Home";
+// import About from "../pages/About";
+
+// // Trek Reports
+// import BaildaraToFenyadeviGhat from "../pages/trek-reports/baildara-to-fenyadevi-ghat";
+
+// const AppRoutes: React.FC = () => (
+//   <Router basename="/trek-reports">
+//     <Layout>
+//       <Routes>
+//         {/* General Pages */}
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+
+//         {/* Trek Reports */}
+//         <Route
+//           path="/report/baildara-to-fenyadevi-ghat"
+//           element={<BaildaraToFenyadeviGhat />}
+//         />
+
+//         {/* Catch-All: 404 Not Found */}
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+//     </Layout>
+//   </Router>
+// );
+
+// export default AppRoutes;
+
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import Layout from "../components/Layout";
 import NotFound from "../components/NotFound";
 import Home from "../pages/Home";
 import About from "../pages/About";
 
-{
-  /*trek report*/
-}
+// Trek Reports
 import BaildaraToFenyadeviGhat from "../pages/trek-reports/baildara-to-fenyadevi-ghat";
 
 const AppRoutes: React.FC = () => (
-  <Layout>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<NotFound />} />
+  <Router>
+    <Layout>
+      <Routes>
+        {/* General Pages */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
 
-      {/* Trek reports */}
-      <Route
-        path="/report/baildara-to-fenyadevi-ghat"
-        element={<BaildaraToFenyadeviGhat />}
-      />
-    </Routes>
-  </Layout>
+        {/* Trek Reports */}
+        <Route
+          path="/report/baildara-to-fenyadevi-ghat"
+          element={<BaildaraToFenyadeviGhat />}
+        />
+
+        {/* Catch-All: 404 Not Found */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Layout>
+  </Router>
 );
 
 export default AppRoutes;
